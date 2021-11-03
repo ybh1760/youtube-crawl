94b84ff337782507e9d92dc2901c25ebbedbb559
= <ybh1760@gmail.com>
2021-11-03 17:48:14 +0900

    chore: add VIDEO_DATA_REGEX, DURATION_REGEX constants

--- constants/youtube.constant.ts
+++ constants/youtube.constant.ts
@@ -1,2 +1,5 @@
-export const YOUTUBE_BASE_URL = 'https://www.youtube.com/watch'
-export const VIEW_COUNT_REGEX = /"viewCount":"([0-9]+)"/
+export const YOUTUBE_BASE_URL = 'https://www.youtube.com/watch';
+
+export const VIEW_COUNT_REGEX = /"viewCount":"([0-9]+)"/;
+export const DURATION_REGEX = /"approxDurationMs":"([0-9]+)"/;
+export const VIDEO_DATA_REGEX = /var ytInitialPlayerResponse/;
